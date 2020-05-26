feb94e3a5ee40fedfe9426b27e0959fb4133226d
kk140906 <23394525+kk140906@users.noreply.github.com>
2020-05-26 23:49:54 +0800

    队列

--- LineaStructure_Array_LinkedList/Queue/Queue.cpp
+++ LineaStructure_Array_LinkedList/Queue/Queue.cpp
@@ -1,8 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct 
+{
+	int* pBase;
+	int front;
+	int rear;
+	int length;
+} Queue;
+
+// 函数声明
+void init(Queue* pQueue, int length);//队列初始化
+void enqueue(Queue* pQueue, int var);
+void show(Queue* pQueue);
+
 int main(void)
 {
-	puts("hello world");
+	Queue q;
+	init(&q,4);
+	enqueue(&q,1);
+	enqueue(&q,2);
+	show(&q);
+	
 	return 0;
+}
+
+void init(Queue* pQueue, int length)
+{
+	pQueue->pBase = (int*)malloc(sizeof(int) * (length + 1));
+	if (!pQueue)
+	{
+		puts("内存分配失败!");
+		exit(-1);
+	}
+	pQueue->length = length + 1; 
+	pQueue->front = 0;
+	pQueue->rear = 0;
+}
+
+void enqueue(Queue* pQueue, int var)
+{
+	pQueue->pBase[pQueue->rear] = var;
+	pQueue->rear = (pQueue->rear + 1) % pQueue->length; 
+}
+
+void show(Queue* pQueue)
+{
+	while(pQueue->front != pQueue->rear)	
+	{
+		printf("%d\t",pQueue->pBase[pQueue->front]);
+		pQueue->front++;
+	}
 }
